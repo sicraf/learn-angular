140746b1cbef9f1ae74082a616f9d475a6658655
sicraf <0325van@gmail.com>
2017-09-08 22:24:05 +0800

    完成服务

--- angular-tour-of-heroes/src/app/app.component.js
+++ angular-tour-of-heroes/src/app/app.component.js
@@ -5,35 +5,38 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 var core_1 = require("@angular/core");
-var HEROES = [
-    { id: 11, name: '影流之镰 凯隐' },
-    { id: 12, name: '逆羽 霞' },
-    { id: 13, name: '幻翎	洛' },
-    { id: 14, name: '青钢影 卡蜜尔' },
-    { id: 15, name: '翠神	艾翁' },
-    { id: 16, name: '暴怒骑士	克烈' },
-    { id: 17, name: '岩雀	塔莉垭' },
-    { id: 18, name: '铸星龙王	奥瑞利安·索尔' },
-    { id: 19, name: '戏命师	烬' },
-    { id: 20, name: '海兽祭司	俄洛伊' }
-];
+var hero_service_1 = require("./hero.service");
 var AppComponent = (function () {
-    function AppComponent() {
+    function AppComponent(heroService) {
+        this.heroService = heroService;
         this.title = '英雄之旅';
-        this.heroes = HEROES;
     }
+    ;
+    AppComponent.prototype.getHeroes = function () {
+        var _this = this;
+        this.heroService.getHeroes().then(function (heroes) { return _this.heroes = heroes; });
+    };
+    AppComponent.prototype.ngOnInit = function () {
+        this.getHeroes();
+    };
     AppComponent.prototype.onSelect = function (hero) {
         this.selectedHero = hero;
     };
+    ;
     return AppComponent;
 }());
 AppComponent = __decorate([
     core_1.Component({
         selector: 'my-app',
         template: "\n    <h1>{{title}}</h1>\n    <h2>\u6211\u7684\u82F1\u96C4</h2>\n    <ul class=\"heroes\">\n      <li *ngFor=\"let hero of heroes\" (click)=\"onSelect(hero)\" [class.selected]=\"hero === selectedHero\">\n        <span class=\"badge\">{{hero.id}}</span> {{hero.name}}\n      </li>\n    </ul>\n    <hero-detail [hero]=\"selectedHero\"></hero-detail>\n  ",
-        styles: ["\n    .selected {\n      background-color: #CFD8DC !important;\n      color: white;\n    }\n    .heroes {\n      margin: 0 0 2em 0;\n      list-style-type: none;\n      padding: 0;\n      width: 15em;\n    }\n    .heroes li {\n      cursor: pointer;\n      position: relative;\n      left: 0;\n      background-color: #EEE;\n      margin: .5em;\n      padding: .3em 0;\n      height: 1.6em;\n      border-radius: 4px;\n      transition: left .1s;\n    }\n    .heroes li.selected:hover {\n      background-color: #BBD8DC !important;\n      color: white;\n    }\n    .heroes li:hover {\n      color: #607D8B;\n      background-color: #DDD;\n      left: .1em;\n    }\n    .heroes .text {\n      position: relative;\n      top: -3px;\n    }\n    .heroes .badge {\n      display: inline-block;\n      font-size: small;\n      color: white;\n      padding: 0.8em 0.7em 0 0.7em;\n      background-color: #607D8B;\n      line-height: 1em;\n      position: relative;\n      left: -1px;\n      top: -4px;\n      height: 1.8em;\n      margin-right: .8em;\n      border-radius: 4px 0 0 4px;\n    }\n  "]
-    })
+        styles: ["\n    .selected {\n      background-color: #CFD8DC !important;\n      color: white;\n    }\n    .heroes {\n      margin: 0 0 2em 0;\n      list-style-type: none;\n      padding: 0;\n      width: 15em;\n    }\n    .heroes li {\n      cursor: pointer;\n      position: relative;\n      left: 0;\n      background-color: #EEE;\n      margin: .5em;\n      padding: .3em 0;\n      height: 1.6em;\n      border-radius: 4px;\n      transition: left .1s;\n    }\n    .heroes li.selected:hover {\n      background-color: #BBD8DC !important;\n      color: white;\n    }\n    .heroes li:hover {\n      color: #607D8B;\n      background-color: #DDD;\n      left: .1em;\n    }\n    .heroes .text {\n      position: relative;\n      top: -3px;\n    }\n    .heroes .badge {\n      display: inline-block;\n      font-size: small;\n      color: white;\n      padding: 0.8em 0.7em 0 0.7em;\n      background-color: #607D8B;\n      line-height: 1em;\n      position: relative;\n      left: -1px;\n      top: -4px;\n      height: 1.8em;\n      margin-right: .8em;\n      border-radius: 4px 0 0 4px;\n    }\n  "],
+        providers: [hero_service_1.HeroService]
+    }),
+    __metadata("design:paramtypes", [hero_service_1.HeroService])
 ], AppComponent);
 exports.AppComponent = AppComponent;
 //# sourceMappingURL=app.component.js.map
--- angular-tour-of-heroes/src/app/app.component.ts
+++ angular-tour-of-heroes/src/app/app.component.ts
@@ -1,19 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 import { Hero } from './hero';
+import { HeroService } from './hero.service';
 
-
-const HEROES: Hero[] = [
-  { id: 11, name: '影流之镰 凯隐' },
-  { id: 12, name: '逆羽 霞' },
-  { id: 13, name: '幻翎	洛' },
-  { id: 14, name: '青钢影 卡蜜尔' },
-  { id: 15, name: '翠神	艾翁' },
-  { id: 16, name: '暴怒骑士	克烈' },
-  { id: 17, name: '岩雀	塔莉垭' },
-  { id: 18, name: '铸星龙王	奥瑞利安·索尔' },
-  { id: 19, name: '戏命师	烬' },
-  { id: 20, name: '海兽祭司	俄洛伊' }
-];
 
 @Component({
   selector: 'my-app',
@@ -76,14 +65,28 @@ const HEROES: Hero[] = [
       margin-right: .8em;
       border-radius: 4px 0 0 4px;
     }
-  `]
+  `],
+  providers: [HeroService]
 })
 
-export class AppComponent  { 
+
+export class AppComponent implements OnInit  { 
   title = '英雄之旅';
   selectedHero : Hero;
-  heroes = HEROES;
+  heroes: Hero[];
+
+
+  constructor(private heroService: HeroService) { };
+
+  getHeroes(): void {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+  
   onSelect(hero: Hero):void{
     this.selectedHero = hero;
-  }
+  };
  }
